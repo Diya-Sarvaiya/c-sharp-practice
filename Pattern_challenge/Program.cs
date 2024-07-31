ed41748d3c3d5a2df11e573033179e6735596e49
Diya Sarvaiya <dsarvaiya4270@gmail.com>
2024-07-31 09:49:23 +0530

    added pattern

---
+++ Pattern_challenge/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern_challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            char[] arr = { 'A', 'B', 'C', 'D', 'E', 'F', 'I', 'J', 'K', 'L' };
+            for (int i = 0; i < n; i++)
+            {
+                int j;
+                for (j = 0; j <= i; j++)
+                {
+                    Console.Write(arr[j]);
+                }
+
+                for (j = i; j > 0; j--)
+                {
+                    Console.Write(arr[j - 1]);
+                }
+
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
